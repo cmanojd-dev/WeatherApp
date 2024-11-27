882c10b59d2093c0fc16686abf8b7b3fe5f99d4e
cmanojd <cmanojd.dev@gmail.com>
2024-11-27 13:53:11 +0530

    updated cities

--- src/Assets/theme/appDataConfig.js
+++ src/Assets/theme/appDataConfig.js
@@ -29,21 +29,134 @@ export const states = [
   {id: 28, name: 'West Bengal'},
 ];
 
-// export const cities = [
-//   {id: 1, name: 'Visakhapatnam', stateId: 1},
-//   {id: 2, name: 'Vijayawada', stateId: 1},
-//   {id: 3, name: 'Guntur', stateId: 1},
-//   {id: 4, name: 'Nellore', stateId: 1},
-//   {id: 5, name: 'Tirupati', stateId: 1},
-//   {id: 6, name: 'Kakinada', stateId: 1},
-//   {id: 7, name: 'Rajahmundry', stateId: 1},
-//   {id: 8, name: 'Kurnool', stateId: 1},
-//   {id: 9, name: 'Anantapur', stateId: 1},
-//   {id: 10, name: 'Warangal', stateId: 1},
-// ];
+export const cities = {
+  1: [
+    {id: 1, name: 'Visakhapatnam', stateId: 1},
+    {id: 2, name: 'Vijayawada', stateId: 1},
+    {id: 3, name: 'Guntur', stateId: 1},
+    {id: 4, name: 'Nellore', stateId: 1},
+    {id: 5, name: 'Tirupati', stateId: 1},
+    {id: 6, name: 'Kakinada', stateId: 1},
+    {id: 7, name: 'Rajahmundry', stateId: 1},
+    {id: 8, name: 'Kurnool', stateId: 1},
+    {id: 9, name: 'Anantapur', stateId: 1},
+    {id: 10, name: 'Warangal', stateId: 1},
+  ],
+  2: [
+    {id: 1, name: 'Itanagar', stateId: 2},
+    {id: 2, name: 'Tawang', stateId: 2},
+    {id: 3, name: 'Pasighat', stateId: 2},
+    {id: 4, name: 'Ziro', stateId: 2},
+    {id: 5, name: 'Bomdila', stateId: 2},
+  ],
+  3: [
+    {id: 1, name: 'Guwahati', stateId: 3},
+    {id: 2, name: 'Dibrugarh', stateId: 3},
+    {id: 3, name: 'Jorhat', stateId: 3},
+    {id: 4, name: 'Silchar', stateId: 3},
+    {id: 5, name: 'Tezpur', stateId: 3},
+    {id: 6, name: 'Tinsukia', stateId: 3},
+    {id: 7, name: 'Nagaon', stateId: 3},
+    {id: 8, name: 'Bongaigaon', stateId: 3},
+    {id: 9, name: 'Goalpara', stateId: 3},
+    {id: 10, name: 'Barpeta', stateId: 3},
+  ],
+  4: [
+    {id: 1, name: 'Patna', stateId: 4},
+    {id: 2, name: 'Gaya', stateId: 4},
+    {id: 3, name: 'Bhagalpur', stateId: 4},
+    {id: 4, name: 'Muzaffarpur', stateId: 4},
+    {id: 5, name: 'Darbhanga', stateId: 4},
+    {id: 6, name: 'Purnia', stateId: 4},
+    {id: 7, name: 'Ara', stateId: 4},
+    {id: 8, name: 'Begusarai', stateId: 4},
+    {id: 9, name: 'Katihar', stateId: 4},
+    {id: 10, name: 'Chapra', stateId: 4},
+  ],
+  5: [
+    {id: 1, name: 'Raipur', stateId: 5},
+    {id: 2, name: 'Bilaspur', stateId: 5},
+    {id: 3, name: 'Durg', stateId: 5},
+    {id: 4, name: 'Bhilai', stateId: 5},
+    {id: 5, name: 'Korba', stateId: 5},
+    {id: 6, name: 'Raigarh', stateId: 5},
+    {id: 7, name: 'Jagdalpur', stateId: 5},
+    {id: 8, name: 'Ambikapur', stateId: 5},
+    {id: 9, name: 'Dhamtari', stateId: 5},
+    {id: 10, name: 'Mahasamund', stateId: 5},
+  ],
+  6: [
+    {id: 1, name: 'Panaji', stateId: 6},
+    {id: 2, name: 'Margao', stateId: 6},
+    {id: 3, name: 'Vasco da Gama', stateId: 6},
+    {id: 4, name: 'Mapusa', stateId: 6},
+    {id: 5, name: 'Ponda', stateId: 6},
+  ],
+  7: [
+    {id: 1, name: 'Ahmedabad', stateId: 7},
+    {id: 2, name: 'Surat', stateId: 7},
+    {id: 3, name: 'Vadodara', stateId: 7},
+    {id: 4, name: 'Rajkot', stateId: 7},
+    {id: 5, name: 'Bhavnagar', stateId: 7},
+    {id: 6, name: 'Vadnagar', stateId: 7},
+    {id: 7, name: 'Junagadh', stateId: 7},
+    {id: 8, name: 'Gandhinagar', stateId: 7},
+    {id: 9, name: 'Anand', stateId: 7},
+    {id: 10, name: 'Nadiad', stateId: 7},
+  ],
+  8: [
+    {id: 1, name: 'Chandigarh', stateId: 8},
+    {id: 2, name: 'Faridabad', stateId: 8},
+    {id: 3, name: 'Gurugram', stateId: 8},
+    {id: 4, name: 'Panipat', stateId: 8},
+    {id: 5, name: 'Ambala', stateId: 8},
+    {id: 6, name: 'Hisar', stateId: 8},
+    {id: 7, name: 'Karnal', stateId: 8},
+    {id: 8, name: 'Rohtak', stateId: 8},
+    {id: 9, name: 'Sonipat', stateId: 8},
+    {id: 10, name: 'Yamunanagar', stateId: 8},
+  ],
+  9: [
+    {id: 1, name: 'Shimla', stateId: 9},
+    {id: 2, name: 'Manali', stateId: 9},
+    {id: 3, name: 'Dharamshala', stateId: 9},
+    {id: 4, name: 'Kullu', stateId: 9},
+    {id: 5, name: 'Solan', stateId: 9},
+    {id: 6, name: 'Kangra', stateId: 9},
+    {id: 7, name: 'Hamirpur', stateId: 9},
+    {id: 8, name: 'Mandi', stateId: 9},
+    {id: 9, name: 'Chamba', stateId: 9},
+    {id: 10, name: 'Bilaspur', stateId: 9},
+  ],
+  10: [
+    {id: 1, name: 'Ranchi', stateId: 10},
+    {id: 2, name: 'Jamshedpur', stateId: 10},
+    {id: 3, name: 'Dhanbad', stateId: 10},
+    {id: 4, name: 'Bokaro Steel City', stateId: 10},
+    {id: 5, name: 'Hazaribagh', stateId: 10},
+    {id: 6, name: 'Giridih', stateId: 10},
+    {id: 7, name: 'Deoghar', stateId: 10},
+    {id: 8, name: 'Chaibasa', stateId: 10},
+    {id: 9, name: 'Ramgarh', stateId: 10},
+    {id: 10, name: 'Koderma', stateId: 10},
+  ],
+};
 
-export const cities = [
-  {id: 1, name: 'Surat', stateId: 7},
-  {id: 2, name: 'Ahmedabad', stateId: 7},
-  {id: 3, name: 'Bhavnagar', stateId: 7},
-];
+// Function to get cities by state
+export function getCitiesByState(stateId) {
+  if (cities[stateId]) {
+    return cities[stateId];
+  } else {
+    return [];
+  }
+}
+
+// Function to find state based on cityId
+export function getStateNameByCityId(cityId) {
+  if (cityId) {
+    const requiredState = states?.filter(state => state?.id === cityId);
+    return requiredState?.[0]?.name ?? '';
+  } else {
+    return '';
+  }
+}
